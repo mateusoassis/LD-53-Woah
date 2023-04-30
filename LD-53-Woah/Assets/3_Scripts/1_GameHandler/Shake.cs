bafafde4075e12fe3c1a993fd2e777604ea4dea4
Mateus Assis <mateusoassis@gmail.com>
2023-04-29 22:13:02 -0300

    Camera shake created, still needing to implement

---
+++ LD-53-Woah/Assets/3_Scripts/1_GameHandler/Shake.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float shakeDuration;
+    public float shakeMagnitude;
+    void Start()
+    {
+        CallShake(shakeDuration, shakeMagnitude);
+    }
+
+    public void CallShake(float duration, float magnitude)
+    {
+        StartCoroutine(ShakeThis(duration, magnitude));
+    }
+
+    public IEnumerator ShakeThis(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsedTime = 0f;
+
+        while(elapsedTime < duration)
+        {
+            float xOffset = Random.Range(-0.5f, 0.5f) * magnitude;
+            float yOffset = Random.Range(-0.5f, 0.5f) * magnitude;
+
+            transform.localPosition = new Vector3(xOffset, yOffset, originalPos.z);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
