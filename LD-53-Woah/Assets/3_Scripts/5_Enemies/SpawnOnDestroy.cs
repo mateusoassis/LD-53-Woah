c43c445fccaf7871886225cd0cf818e4d5aaa4c8
Mateus Assis <mateusoassis@gmail.com>
2023-05-01 03:59:53 -0300

    Enemy colliders were like 0.0001 in size, now they're on normal size

--- LD-53-Woah/Assets/3_Scripts/5_Enemies/SpawnOnDestroy.cs
+++ LD-53-Woah/Assets/3_Scripts/5_Enemies/SpawnOnDestroy.cs
@@ -10,5 +10,6 @@ public class SpawnOnDestroy : MonoBehaviour
     {
         GameObject fatPlus = Instantiate(fatPlusPrefab, this.transform.position, Quaternion.identity);
         GetComponent<EnemyBehaviour>().playerStatus.enemyCounter++;
+        
     }
 }
