601025be0e4e57eed672e9687078be2d66ff2efa
Mateus Assis <mateusoassis@gmail.com>
2023-05-01 01:19:56 -0300

    Destroy reworked, now it's a goddamn laser

--- LD-53-Woah/Assets/3_Scripts/4_Player/Skills/Destruir.cs
+++ LD-53-Woah/Assets/3_Scripts/4_Player/Skills/Destruir.cs
@@ -6,11 +6,23 @@ public class Destruir : MonoBehaviour
 {
     [SerializeField] private PlayerSkills skill;
     [SerializeField] private PlayerStatus playerStatus;
+    [SerializeField] private GameObject parent;
+    [SerializeField] private Transform rotationTransform;
+    [SerializeField] private float totalRange;
+    // max 180
     void Awake()
     {
         skill.ZeroCooldownTimer();
         transform.localScale = new Vector3(skill.skillArray.skillRadius, skill.skillArray.skillRadius, 1f);
         playerStatus = GameObject.Find("DestinationPoint").GetComponent<PlayerStatus>();
+        Destroy(parent, skill.skillArray.skillCooldown);
+        GameObject.Find("SoundManager").GetComponent<AudioClipManager>().PlayOneShot("DealDamage");
+    }
+
+    void Start()
+    {
+        float u = Random.Range(-totalRange/2, totalRange/2);
+        rotationTransform.Rotate(0f, 0f, u);
     }
 
     void Update()
@@ -26,7 +38,7 @@ public class Destruir : MonoBehaviour
         if(skill.skillArray.skillCooldownTimer > skill.skillArray.skillCooldown)
         {
             skill.skillArray.canCast = true;
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
         }
     }
 
