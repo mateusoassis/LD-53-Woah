5781f1c8bd0993300428abad3111c8eef68dcd83
DESKTOP-2G0U64C\George <ghms.98@gmail.com>
2023-05-01 21:32:31 -0300

    Fuck

---
+++ LD-53-Woah/Assets/StartGameScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void StartGame()
+    {
+        SceneManager.LoadScene("1_Scenes/2_GameScene 1");
+    }
+}
